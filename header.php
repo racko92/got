49ab0e0ebf5817d75dcdbc119f3d2bc249ef0088
racko92 <vladimir.rackovic.92@gmail.com>
2017-07-14 16:44:08 +0200

    Day one

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Game of Thrones Website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<header>
+	<img src="image/Game-of-Thrones-Logo-Free-Download-PNG.png" class="logo">
+	<nav>
+		<a href="#">Home</a>
+		<a href="#">Content</a>
+		<a href="#">Link 3</a>
+		<a href="#">About</a>
+		<a href="#">Contact</a>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+	include 'header.php';
+?>
+<section class="intro">
+	<video poster="image/GOT S7 Background.png" class="bgvid" playsinline autoplay muted loop>
+	  <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+	<source src="video/Game of Thrones Season 7 Official Tease Sigils.mp4" type="video/mp4">
+	<source src="video/Game of Thrones Season 7 Official Tease Sigils" type="video/webm">
+	</video>
+	<div class="intro-content">
+		<h2>Dummy Header</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</section>
+
+<?php 
+	include 'footer.php';
+?>
